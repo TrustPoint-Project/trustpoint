8d9cac5c90553cd8765395fb3d047ef7d4b5e266
Mario Kemper <mario.kemper@achelos.de>
2024-02-13 09:27:18 +0100

    Use own labels instead of auto generated ones

--- trustpoint/sysconf/forms.py
+++ trustpoint/sysconf/forms.py
@@ -1,14 +1,23 @@
 from django import forms
 from .models import NetworkConfig, NTPConfig
+from django.utils.translation import gettext_lazy as _
 
 class NTPConfigForm(forms.ModelForm):
     class Meta:
         model = NTPConfig
         fields = '__all__'
+        labels = {
+            "ntp_server_address": _("NTP Server Address"),
+        }
 
 class NetworkConfigForm(forms.ModelForm):
    
     class Meta:
         model = NetworkConfig
         fields = '__all__'
-        
+        labels = {
+            "static_ip_address": _("Static IP Address"),
+            "dhcp": _("DHCP"),
+            "netmask": _("Netmask"),
+            "gateway": _("Gateway")
+        }
