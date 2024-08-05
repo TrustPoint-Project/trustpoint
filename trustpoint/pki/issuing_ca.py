394bf79820f81fd186c65472f903d729cef95f5b
AlexHx8472 <alexander.harig@campus-schwarzwald.de>
2024-08-05 16:41:37 +0200

    sign_crl template on IssuingCa

--- trustpoint/pki/issuing_ca.py
+++ trustpoint/pki/issuing_ca.py
@@ -1,7 +1,8 @@
 from __future__ import annotations
 
 from abc import ABC, abstractmethod
-from .serializer import CertificateSerializer, CertificateCollectionSerializer, PublicKeySerializer
+from .serializer import CertificateSerializer, CertificateCollectionSerializer, PublicKeySerializer, \
+    PrivateKeySerializer
 # from devices.models import Device
 
 
@@ -12,6 +13,8 @@ if TYPE_CHECKING:
     from typing import Union
     from .models import CertificateModel, IssuingCaModel
     from cryptography.hazmat.primitives.asymmetric import rsa, ec, ed448, ed25519
+    from cryptography.hazmat.primitives import hashes
+    from cryptography import x509
     PublicKey = Union[rsa.RSAPublicKey, ec.EllipticCurvePublicKey, ed448.Ed448PublicKey, ed25519.Ed25519PublicKey]
 
 
@@ -33,11 +36,20 @@ class IssuingCa(ABC):
 
 class UnprotectedLocalIssuingCa(IssuingCa):
 
+    _issuing_ca_model: IssuingCaModel
+    _private_key_serializer: PrivateKeySerializer
+
     def __init__(self, issuing_ca_model: IssuingCaModel) -> None:
         self._issuing_ca_model = issuing_ca_model
+        self._private_key_serializer = self._get_private_key_serializer()
 
-    def sign_crl(self, crl):
-        pass
+    def _get_private_key_serializer(self) -> PrivateKeySerializer:
+        return PrivateKeySerializer.from_string(self._issuing_ca_model.private_key_pem)
+
+    def generate_crl(self, crl_builder: x509.CertificateRevocationListBuilder) -> x509.CertificateRevocationList:
+        # TODO: this should not take an crl_builder object, but instead get the crl information required from the
+        # TODO: issuing ca model / crl model and sign the crl as below.
+        return crl_builder.sign(private_key=self._private_key_serializer.as_crypto(), algorithm=hashes.SHA256())
 
     # def issue_ldevid(self, device: Device):
     #     pass
