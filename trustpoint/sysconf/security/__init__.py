2e6065eceb5ec87c4db7b7d8ab498c5ff074c1da
dik <dominik.isaak@achelos.de>
2024-08-23 12:39:32 +0200

    Adding security level implementatios #75

--- trustpoint/sysconf/security/__init__.py
+++ trustpoint/sysconf/security/__init__.py
@@ -1,4 +1,5 @@
 class SecurityFeatures:
+    """A class that defines various security features used throughout the application."""
     LOCAL_ROOT_CA = 'local root ca'
     LOG_ACCESS = 'Log Access'
     ISSUING_CA_TABLE_VIEW = 'Acces to IssuingCaTableView'
