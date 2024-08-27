b95da89edaa5004aa93f7b6b2f5600593455ff3f
dik <dominik.isaak@achelos.de>
2024-08-27 13:38:27 +0200

    Make SecurityFeatures an enum

--- trustpoint/sysconf/security/__init__.py
+++ trustpoint/sysconf/security/__init__.py
@@ -1,5 +1,7 @@
-class SecurityFeatures:
+from enum import Enum
+
+
+class SecurityFeatures(Enum):
     """A class that defines various security features used throughout the application."""
     LOCAL_ROOT_CA = 'local root ca'
     LOG_ACCESS = 'Log Access'
-    ISSUING_CA_TABLE_VIEW = 'Acces to IssuingCaTableView'
