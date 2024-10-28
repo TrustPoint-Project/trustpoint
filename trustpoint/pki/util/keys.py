20b6e1150ba56db0ddda12769927ec8e2a5530a1
AlexHx8472 <alex_mail@posteo.de>
2024-10-28 17:09:00 +0100

    KeyAlgorithm fix

--- trustpoint/pki/util/keys.py
+++ trustpoint/pki/util/keys.py
@@ -19,7 +19,7 @@ class AutoGenPkiKeyAlgorithm(models.TextChoices):
     # omitting the rest of the choices as an example that Auto Gen PKI doesn't have to support all key algorithms
 
     def to_key_algorithm(self) -> KeyAlgorithm:
-        return KeyAlgorithm(self.value)
+        return KeyAlgorithm(str(self))
 
 class KeyGenerator:
     def __init__(self, algorithm: KeyAlgorithm):
