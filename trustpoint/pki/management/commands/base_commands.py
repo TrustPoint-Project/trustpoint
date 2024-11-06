5c1ca62fc6a5e249860501358aaa54e7c537503b
AlexHx8472 <alex_mail@posteo.de>
2024-11-06 15:46:34 +0100

    aki, ski in base commands

--- trustpoint/pki/management/commands/base_commands.py
+++ trustpoint/pki/management/commands/base_commands.py
@@ -49,6 +49,12 @@ class CertificateCreationCommandMixin:
         builder = builder.add_extension(
             x509.BasicConstraints(ca=True, path_length=None), critical=True,
         )
+        builder = builder.add_extension(
+            x509.SubjectKeyIdentifier.from_public_key(public_key), critical=False
+        )
+        builder = builder.add_extension(
+            x509.AuthorityKeyIdentifier.from_issuer_public_key(public_key), critical=False
+        )
         certificate = builder.sign(
             private_key=private_key, algorithm=hashes.SHA256(),
         )
@@ -84,6 +90,9 @@ class CertificateCreationCommandMixin:
         )
         builder = builder.add_extension(
             x509.SubjectKeyIdentifier.from_public_key(public_key), critical=False
+        )
+        builder = builder.add_extension(
+            x509.AuthorityKeyIdentifier.from_issuer_public_key(issuer_private_key.public_key()), critical=False
         )
         certificate = builder.sign(
             private_key=issuer_private_key, algorithm=hashes.SHA256(),
