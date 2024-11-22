b1e150053d86fdd21488cd9d40d890ca67d779e7
AlexHx8472 <alex_mail@posteo.de>
2024-11-22 12:00:58 +0100

    changes to the user app required for the wizard

--- trustpoint/users/views.py
+++ trustpoint/users/views.py
@@ -1,1 +1,35 @@
 """Views for the users application."""
+from __future__ import annotations
+
+from django.contrib.auth.views import LoginView
+from django.contrib import messages
+
+
+from setup_wizard import SetupWizardState
+from setup_wizard.views import StartupWizardRedirect
+
+
+class TrustpointLoginView(LoginView):
+    http_method_names = ['get', 'post']
+
+    def get(self, *args, **kwargs):
+        # clears all messages
+        for _ in messages.get_messages(self.request):
+            pass
+
+        wizard_state = SetupWizardState.get_current_state()
+        if wizard_state == SetupWizardState.WIZARD_COMPLETED:
+            return super().get(*args, **kwargs)
+
+        return StartupWizardRedirect.redirect_by_state(wizard_state)
+
+    def post(self, *args, **kwargs):
+        # clears all messages
+        for _ in messages.get_messages(self.request):
+            pass
+
+        wizard_state = SetupWizardState.get_current_state()
+        if wizard_state == SetupWizardState.WIZARD_COMPLETED:
+            return super().post(*args, **kwargs)
+
+        return StartupWizardRedirect.redirect_by_state(wizard_state)
