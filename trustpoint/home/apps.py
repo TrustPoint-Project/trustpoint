ec9240981b33d14c1f2c3276e0df141d7a2863b4
Rohit Bohara <r.bohara@asvin.io>
2024-11-26 17:27:37 +0100

    fix ruff check error

--- trustpoint/home/apps.py
+++ trustpoint/home/apps.py
@@ -1,6 +1,9 @@
+"""Django apps module which defines the Home app configuration."""
+
 from django.apps import AppConfig
 
 
 class HomeConfig(AppConfig):
+    """Home app configuration."""
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'home'
