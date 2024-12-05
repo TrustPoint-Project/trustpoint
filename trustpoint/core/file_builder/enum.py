a970216336ec44aeeb4ef24a2a36d202a1ed0b89
AlexHx8472 <alex_mail@posteo.de>
2024-12-05 16:49:05 +0100

    core.file_builder.enum rework

--- trustpoint/core/file_builder/enum.py
+++ trustpoint/core/file_builder/enum.py
@@ -12,10 +12,15 @@ __all__ = [
 
 class ArchiveFormat(enum.Enum):
 
+    mime_type: str
+    file_extension: str
+
     ZIP: str = ('zip', 'application/zip', '.zip')
     TAR_GZ: str = ('tar_gz', 'application/gzip', '.tar.gz')
 
-    def __new__(cls, value: str, mime_type: str, file_extension: str) -> ArchiveFormat:
+    def __new__(cls, value: None | str, mime_type: str = '', file_extension: str = '') -> ArchiveFormat:
+        if value is None:
+            raise ValueError('None is not a valid archive format.')
         obj = object.__new__(cls)
         obj._value_ = value
         obj.mime_type = mime_type
@@ -25,12 +30,18 @@ class ArchiveFormat(enum.Enum):
 
 
 class CertificateFileFormat(enum.Enum):
+
+    mime_type: str
+    file_extension: str
+
     PEM: str = ('pem', 'application/x-pem-file', '.pem')
     DER: str = ('der', 'application/pkix-cert', '.cer')
     PKCS7_PEM: str = ('pkcs7_pem', 'application/x-pkcs7-certificates', '.p7b')
     PKCS7_DER: str = ('pkcs7_der', 'application/x-pkcs7-certificates', '.p7b')
 
-    def __new__(cls, value: str, mime_type: str, file_extension: str) -> CertificateFileFormat:
+    def __new__(cls, value: None | str, mime_type: str = '', file_extension: str = '') -> CertificateFileFormat:
+        if value is None:
+            raise ValueError('None is not a valid file format.')
         obj = object.__new__(cls)
         obj._value_ = value
         obj.mime_type = mime_type
