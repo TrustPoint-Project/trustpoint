573c0fb0db97531ae6d9dccf3511bb2a9fe2a014
AlexHx8472 <alex_mail@posteo.de>
2025-01-16 17:57:27 +0100

    devices/forms.py ruff & mypy pass

--- trustpoint/devices/forms.py
+++ trustpoint/devices/forms.py
@@ -1,31 +1,39 @@
+"""Forms exclusively used in the device application."""
+
 from __future__ import annotations
 
 import ipaddress
-from typing import TYPE_CHECKING
+from typing import Any, cast
 
 from django import forms
 from django.utils.translation import gettext_lazy as _
 
 from devices.models import DeviceModel, IssuedCredentialModel
 
-if TYPE_CHECKING:
-    from typing import Any
+PASSWORD_MIN_LENGTH = 12
 
 
 class IssueDomainCredentialForm(forms.Form):
+    """Form to issue a new domain credential."""
+
     common_name = forms.CharField(max_length=255, label=_('Common Name'), required=True, disabled=True)
     domain_component = forms.CharField(max_length=255, label=_('Domain Component'), required=True, disabled=True)
     serial_number = forms.CharField(max_length=255, label=_('Serial Number'), required=True, disabled=True)
 
 
 class CredentialDownloadForm(forms.Form):
+    """Form to download a credential."""
+
     password = forms.CharField(
-        label=_('Password'), widget=forms.PasswordInput, help_text=_('Must be at least 12 characters long.')
+        label=_('Password'),
+        widget=forms.PasswordInput,
+        help_text=_('Must be at least %d characters long.') % PASSWORD_MIN_LENGTH,
     )
     confirm_password = forms.CharField(label=_('Confirm Password'), widget=forms.PasswordInput)
 
     def clean(self) -> dict[str, Any]:
-        cleaned_data = super().clean()
+        """Checks if the passwords match and if the password is long enough."""
+        cleaned_data = cast(dict[str, Any], super().clean())
         password = cleaned_data.get('password')
         confirm_password = cleaned_data.get('confirm_password')
 
@@ -33,13 +41,15 @@ class CredentialDownloadForm(forms.Form):
             if password != confirm_password:
                 self.add_error('confirm_password', _('Passwords do not match.'))
 
-            if len(password) < 12:
-                self.add_error('password', _('Password must be at least 12 characters long.'))
+            if len(password) < PASSWORD_MIN_LENGTH:
+                self.add_error('password', _('Password must be at least %d characters long.') % PASSWORD_MIN_LENGTH)
 
         return cleaned_data
 
 
 class IssueTlsClientCredentialForm(forms.Form):
+    """Form to issue a new TLS client credential."""
+
     def __init__(self, *args: Any, device: DeviceModel, **kwargs: Any) -> None:
         """Overwrite the constructor to accept the current device instance."""
         self.device = device
@@ -56,7 +66,8 @@ class IssueTlsClientCredentialForm(forms.Form):
     validity = forms.IntegerField(label=_('Validity (days)'), initial=10, required=True)
 
     def clean_common_name(self) -> str:
-        common_name = self.cleaned_data['common_name']
+        """Checks the common name."""
+        common_name = cast(str, self.cleaned_data['common_name'])
         if IssuedCredentialModel.objects.filter(common_name=common_name, device=self.device).exists():
             err_msg = (
                 f'Credential with common name {common_name} ' f'already exists for device {self.device.unique_name}.'
@@ -64,8 +75,9 @@ class IssueTlsClientCredentialForm(forms.Form):
             raise forms.ValidationError(err_msg)
         return common_name
 
-    def clean_validity(self):
-        validity = self.cleaned_data['validity']
+    def clean_validity(self) -> int:
+        """Checks the validity."""
+        validity = cast(int, self.cleaned_data['validity'])
         if validity <= 0:
             err_msg = _('Validity must be a positive integer.')
             raise forms.ValidationError(err_msg)
@@ -73,6 +85,8 @@ class IssueTlsClientCredentialForm(forms.Form):
 
 
 class IssueTlsServerCredentialForm(forms.Form):
+    """Form to issue a new TLS server credential."""
+
     def __init__(self, *args: Any, device: DeviceModel, **kwargs: Any) -> None:
         """Overwrite the constructor to accept the current device instance."""
         self.device = device
@@ -92,21 +106,26 @@ class IssueTlsServerCredentialForm(forms.Form):
     )
 
     def clean_common_name(self) -> str:
-        common_name = self.cleaned_data['common_name']
+        """Checks the common name."""
+        common_name = cast(str, self.cleaned_data['common_name'])
         if IssuedCredentialModel.objects.filter(common_name=common_name, device=self.device).exists():
-            err_msg = (
-                f'Credential with common name {common_name} ' f'already exists for device {self.device.unique_name}.'
+            err_msg = _('Credential with common name %s already exists for device %s.') % (
+                common_name,
+                self.device.unique_name,
             )
             raise forms.ValidationError(err_msg)
         return common_name
 
-    def clean_validity(self):
-        validity = self.cleaned_data['validity']
+    def clean_validity(self) -> int:
+        """Checks the validity."""
+        validity = cast(int, self.cleaned_data['validity'])
         if validity <= 0:
-            raise forms.ValidationError('Validity must be a positive integer.')
+            err_msg = _('Validity must be a positive integer.')
+            raise forms.ValidationError(err_msg)
         return validity
 
-    def clean_ipv4_addresses(self):
+    def clean_ipv4_addresses(self) -> list[ipaddress.IPv4Address]:
+        """Checks the IPv4 addresses."""
         data = self.cleaned_data['ipv4_addresses'].strip()
         if not data:
             return []
@@ -114,10 +133,12 @@ class IssueTlsServerCredentialForm(forms.Form):
         addresses = data.split(',')
         try:
             return [ipaddress.IPv4Address(address.strip()) for address in addresses if address.strip() != '']
-        except ipaddress.AddressValueError:
-            raise forms.ValidationError('Contains an invalid IPv4-Address.')
+        except ipaddress.AddressValueError as exception:
+            err_msg = _('Contains an invalid IPv4-Address.')
+            raise forms.ValidationError(err_msg) from exception
 
-    def clean_ipv6_addresses(self):
+    def clean_ipv6_addresses(self) -> list[ipaddress.IPv6Address]:
+        """Checks the IPv6 addresses."""
         data = self.cleaned_data['ipv6_addresses'].strip()
         if not data:
             return []
@@ -125,10 +146,12 @@ class IssueTlsServerCredentialForm(forms.Form):
         addresses = data.split(',')
         try:
             return [ipaddress.IPv6Address(address.strip()) for address in addresses if address.strip() != '']
-        except ipaddress.AddressValueError:
-            raise forms.ValidationError('Contains an invalid IPv6-Address.')
+        except ipaddress.AddressValueError as exception:
+            err_msg = _('Contains an invalid IPv6-Address.')
+            raise forms.ValidationError(err_msg) from exception
 
-    def clean_domain_names(self):
+    def clean_domain_names(self) -> list[str]:
+        """Checks the domain names."""
         data = self.cleaned_data['domain_names'].strip()
         if not data:
             return []
@@ -137,10 +160,13 @@ class IssueTlsServerCredentialForm(forms.Form):
         # TODO(AlexHx8472): Check for valid domains.
         return [domain_name.strip() for domain_name in domain_names if domain_name.strip() != '']
 
-    def clean(self):
-        cleaned_data = super().clean()
+    def clean(self) -> dict[str, Any]:
+        """Checks that at least one of IPv4, IPv6 and Domain Names is set."""
+        cleaned_data = cast(dict[str, Any], super().clean())
         ipv4_addresses = cleaned_data.get('ipv4_addresses')
         ipv6_addresses = cleaned_data.get('ipv6_addresses')
         domain_names = cleaned_data.get('domain_names')
         if not (ipv4_addresses or ipv6_addresses or domain_names):
-            raise forms.ValidationError('At least one SAN entry is required.')
+            err_msg = _('At least one SAN entry is required.')
+            raise forms.ValidationError(err_msg)
+        return cleaned_data
