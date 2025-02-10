d13fd71e130690b8df6f5e17977d819fbecf263e
Sven Bergmann <sven.bergmann@achelos.de>
2025-02-10 15:48:59 +0100

    Small change to trigger pipeline.

--- trustpoint/features/environment.py
+++ trustpoint/features/environment.py
@@ -1,5 +1,5 @@
 
-# Environment file which sets up data before running a scenario.
+# Environment file, which sets up data before running a scenario.
 
 # def before_all(context):
 # Set up test environment
