e8b0709712fb4d8eff781b319cdc71913f3c168d
AlexHx8472 <alex_mail@posteo.de>
2025-02-21 14:44:11 +0100

    cmp.urls : ruff & mypy pass

--- trustpoint/cmp/urls.py
+++ trustpoint/cmp/urls.py
@@ -1,3 +1,5 @@
+"""URLs for the Django CMP Application."""
+
 from django.urls import path
 
 from cmp import views
@@ -5,23 +7,16 @@ from cmp import views
 app_name = 'cmp'
 
 urlpatterns = [
-path(
+    path(
         'initialization/<str:domain>/<str:template>/',
         views.CmpInitializationRequestView.as_view(),
-        name='certification-template'
+        name='certification-template',
     ),
-    path(
-        'initialization/<str:domain>/',
-        views.CmpInitializationRequestView.as_view(),
-        name='initialization'
-    ),
+    path('initialization/<str:domain>/', views.CmpInitializationRequestView.as_view(), name='initialization'),
     path(
         'certification/<str:domain>/<str:template>/',
         views.CmpCertificationRequestView.as_view(),
-        name='certification-template'
+        name='certification-template',
     ),
-    path(
-        'certification/<str:domain>/',
-         views.CmpCertificationRequestView.as_view(),
-         name='certification')
+    path('certification/<str:domain>/', views.CmpCertificationRequestView.as_view(), name='certification'),
 ]
